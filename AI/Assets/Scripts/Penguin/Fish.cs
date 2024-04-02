f9ea553b5e8b2de515e68c6dc8929bab1d13bf5e
ahslxjvhapt12 <likjhee1@gmail.com>
2024-04-02 18:55:37 +0900

    Penguin

--- AI/Assets/Scripts/Penguin/Fish.cs
+++ AI/Assets/Scripts/Penguin/Fish.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float fishSpeed;
+    private float nextActionTime = -1f;
+    private Vector3 targetposition;
+
+    private void FixedUpdate()
     {
-        
+        Swim();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Swim()
     {
-        
+        if (Time.fixedTime >= nextActionTime)
+        {
+            // 새로운 위치를 타겟팅하고 nextActionTime을 다시 세팅한다.
+            fishSpeed = Random.Range(0.1f, 0.8f);
+            targetposition = PenguinArea.ChooseRandomPosition(transform.parent.position, 100f, 260f, 2f, 13f);
+            transform.rotation = Quaternion.LookRotation(targetposition - transform.position);
+
+            float timeToTarget = Vector2.Distance(transform.position, targetposition) / fishSpeed; ;
+            nextActionTime = Time.fixedTime + timeToTarget;
+        }
+        else
+        {
+            // 타겟팅된 위치를 향해 이동한다
+            transform.position += transform.forward * Time.fixedDeltaTime * fishSpeed;
+        }
     }
 }
