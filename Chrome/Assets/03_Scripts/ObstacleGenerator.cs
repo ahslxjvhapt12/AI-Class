14d77d25eb16ea7c42b76bfe54fd95e3e2cc29f9
ahslxjvhapt12 <likjhee1@gmail.com>
2024-04-29 18:52:14 +0900

    ?

--- Chrome/Assets/03_Scripts/ObstacleGenerator.cs
+++ Chrome/Assets/03_Scripts/ObstacleGenerator.cs
@@ -1,15 +1,29 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    List<GameObject> obstacles = new List<GameObject>();
-    [SerializeField] Transform spawnPoint;
+    public List<GameObject> obstacles = new List<GameObject>();
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnObstacle), 2f, 2f);
+    }
 
     public void SpawnObstacle()
     {
-
+        int idx = Random.Range(0, obstacles.Count - 1);
+        var obj = Instantiate(obstacles[idx], transform.position, Quaternion.identity);
+        // 호박
+        if (idx == 0)
+        {
+            obj.transform.position = transform.position + new Vector3(10, -3f, 0);
+        }
+        // 유령
+        else
+        {
+            obj.transform.position = transform.position + new Vector3(10, -1.5f, 0);
+        }
     }
 
 
